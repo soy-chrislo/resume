d18dfcd74d2700964f9f8b70ba4080a1852e958e
chrislo <green13-16l@hotmail.com>
2024-01-23 13:55:21 -0500

    correction url

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  resolve: {
-    alias: {
-      '@': './src'
-    }
-  }
+  base: '/vite-deploy-demo/'
 })
